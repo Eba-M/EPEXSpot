17cd4760e44e0785b20015bdafd852cbbe575721
Eberhard Mayer <eberhard.mayer@gmx.net>
2024-12-30 20:49:00 +0100

    Update main.py
    Anpassungen für die Integration in E3DC-Control
    Die URL wird in der nächsten Version per Argument an
    EPEXSpot übergeben und das Ergebnis im Programm weiterverarbeitet.

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
-# This is a sample Python script.
+from bs4 import BeautifulSoup
+import requests
+main_url = "https://www.epexspot.com/en/market-data?market_area=DE-LU&delivery_date=2024-12-31&modality=Aucti$
+print (main_url)
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Ch$
+req = requests.get(main_url,headers=headers)
+print (req.text)
+soup = BeautifulSoup(req.text, "html.parser")
+# Finding the main title tag.
+title = soup.find("table", class_="table-01 table-length-1")
+line = title.contents
+print(len(line))
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    from bs4 import BeautifulSoup
-    import requests
-
-    main_url = "http://www.epexspot.com/de/marktdaten/dayaheadauktion"
-#    main_url = "https: // www.epexspot.com / en / market - data?data_mode = table & modality = Auction & sub_modality = DayAhead & market_area = DE - LU & product = 60 & delivery_date = 2020 - 11 - 01 & trading_date = 2020 - 10 - 31"
-#    main_url = "https: // www.epexspot.com / en / market - data?market_area = DE - LU & trading_date = 2020 - 10 - 31 & delivery_date = 2020 - 11 - 01 & underlying_year = & modality = Auction & sub_modality = DayAhead & product = 60 & data_mode = table & period ="
-#    main_url = "https: // www.epexspot.com / en / market - data?market_area = DE - LU & trading_date = 2020 - 10 - 31 & delivery_date = 2020 - 11 - 01 & underlying_year = & modality = Auction & sub_modality = DayAhead & product = 60 & data_mode = table & period ="
-#    req = requests.get(main_url)
-#    main_url = "https://www.epexspot.com/en/market-data?market_area=AT&trading_date=2020-10-31&delivery_date=2020-11-01&underlying_year=&modality=Auction"
-    main_url = "https://www.epexspot.com/en/market-data?market_area=DE-LU&trading_date=2020-11-15&delivery_date=2020-11-16&modality=Auction&sub_modality=DayAhead&product=60&data_mode=table"
-    req = requests.get(main_url)
-    soup = BeautifulSoup(req.text, "html.parser")
-
-    # Finding the main title tag.
-    title = soup.find("table", class_="table-01 table-length-1")
-    line = title.contents
-    print(len(line))
-
-    td = title.find_all("tr", class_="child")
+td = title.find_all("tr", class_="child")
 # für jedes Element
-    y = 0;
-    for x in td:
+y = 0;
+for x in td:
         print(y,end=' ');print(x.contents[7].string)
         y=y+1
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
