67c68c8e16ae57d76135a23cbfa5beb54c50f676
Eba_M <eberhard.mayer@gmx.net>
2025-01-06 17:44:16 +0100

    Liest automatisch die Börsenpreise des nächsten Tages aus. Bitte erst kurs vor 13:00 aufrufen, sonst werden die Daten vom 1. Januar dargestellt

--- main.py
+++ main.py
@@ -10,20 +10,26 @@ mydate = date + timedelta(days=1)
 print (mydate)
 main_url = "https://www.epexspot.com/en/market-data?market_area=DE-LU&delivery_date="
 main_url = main_url + str(mydate) + "&modality=Auction&sub_modality=DayAhead&product=60&data_mode=table"
-
 print (main_url)
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
 req = requests.get(main_url,headers=headers)
+
 print (req.text)
 soup = BeautifulSoup(req.text, "html.parser")
 # Finding the main title tag.
 title = soup.find("table", class_="table-01 table-length-1")
-line = title.contents
-print(len(line))
+if title.cdata_list_attributes is None:
+        print ('keine Werte gefunden')
+else:
+        line = title.contents
+        print(len(line))
 
-td = title.find_all("tr", class_="child")
+        td = title.find_all("tr", class_="child")
 # für jedes Element
-y = 0;
-for x in td:
-        print(y,end=' ');print(x.contents[7].string)
-        y=y+1
+        y = 0;
+        for x in td:
+                print(y,end=' ');print(x.contents[7].string)
+                y=y+1
+#main_url = "https://www.eex.com/de/marktdaten/market-data-hub/strom/futures#%7B%22snippetpicker%22%3A%2228%22%7D"
+#req = requests.get(main_url,headers=headers)
+#print (req.text)
