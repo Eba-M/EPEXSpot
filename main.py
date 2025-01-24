aedd9c3172f3089651387864a1db53bda08c328a
Eba_M <eberhard.mayer@gmx.net>
2025-01-24 22:24:05 +0100

    Liest automatisch die Börsenpreise des nächsten Tages aus. Bitte erst kurz vor 13:00 aufrufen, sonst werden die Daten vom 1. Januar dargestellt Ergebnis in epexspot.txt

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import sys
-from bs4 import BeautifulSoup
+from typing import Any
+
+from bs4 import BeautifulSoup, PageElement
 import requests
 #print (sys.argv[1])
 from datetime import date
 from datetime import timedelta
 date = date.today()
-print (date)
+#print (date)
 mydate = date + timedelta(days=1)
-print (mydate)
-main_url = "https://www.epexspot.com/en/market-data?market_area=DE-LU&delivery_date="
+fp = open("epexspot.txt","w")
+fp.write(str(mydate)+'\n')
+main_url = "http://www.epexspot.com/en/market-data?market_area=DE-LU&delivery_date="
 main_url = main_url + str(mydate) + "&modality=Auction&sub_modality=DayAhead&product=60&data_mode=table"
 print (main_url)
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
-req = requests.get(main_url,headers=headers)
+#headers = {'Mozilla/5.0 (platform; rv:gecko-version) Gecko/gecko-trail Firefox/firefox-version'}
+try:
+        req = requests.get(main_url,headers=headers,verify=False)
+except requests.exceptions.SSLError as err:
+        print(err)
+else:
+        print("SSL Certificate is valid")
 
-print (req.text)
+#print (req.text)
 soup = BeautifulSoup(req.text, "html.parser")
 # Finding the main title tag.
 title = soup.find("table", class_="table-01 table-length-1")
@@ -22,14 +31,19 @@ if title.cdata_list_attributes is None:
         print ('keine Werte gefunden')
 else:
         line = title.contents
-        print(len(line))
+#        print(len(line))
 
         td = title.find_all("tr", class_="child")
 # für jedes Element
         y = 0;
         for x in td:
-                print(y,end=' ');print(x.contents[7].string)
+                fp.write(str(y)+' '+str(x.contents[7].string)+'\n')
                 y=y+1
-#main_url = "https://www.eex.com/de/marktdaten/market-data-hub/strom/futures#%7B%22snippetpicker%22%3A%2228%22%7D"
+        fp.close()
+#main_url = "https://www.eex.com/de/marktdaten/market-data-hub/strom/futures#%7B%22snippetpicker%22%3A%2228%22%7D:~:text=Wochenende-,Tag,-Base"
 #req = requests.get(main_url,headers=headers)
 #print (req.text)
+#soup = BeautifulSoup(req.text, "html.parser")
+#td = soup.find_all("tr", class_="child")
+
+#print (td)
