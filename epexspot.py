68425175bb780d348766ffebc6ee0b1836ed548d
Eba_M <eberhard.mayer@gmx.net>
2026-08-02 19:57:27 +0200

    Überpüfung ob korrekte EPEX-Seite

--- epexspot.py
+++ epexspot.py
@@ -1,4 +1,5 @@
 import sys
+#import pandas as pd
 mday = 1
 marketarea = "DE-LU"
 n = len(sys.argv)
@@ -14,7 +15,7 @@ from datetime import date
 from datetime import timedelta
 date = date.today()
 #print (date)
-mydate = date + timedelta(days=mday)
+mydate: date = date + timedelta(days=mday)
 fp = open("epexspot.txt","w")
 fp.write(str(mydate)+'\n')
 main_url = "http://www.epexspot.com/en/market-data?market_area="
@@ -22,7 +23,7 @@ main_url = main_url + marketarea + "&delivery_date="
 main_url = main_url + str(mydate) + "&modality=Auction&sub_modality=DayAhead&product=15&data_mode=table"
 print (main_url)
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
-#headers = {'Mozilla/5.0 (platform; rv:gecko-version) Gecko/gecko-trail Firefox/firefox-version'}
+#headers = {'User-Agent': 'Mozilla/5.0 (platform; rv:gecko-version) Gecko/gecko-trail Firefox/firefox-version'}
 try:
         req = requests.get(main_url,headers=headers,verify=False)
 except requests.exceptions.SSLError as err:
@@ -30,16 +31,43 @@ except requests.exceptions.SSLError as err:
 else:
         print("SSL Certificate is valid")
 
-#print (req.text)
+print (req.text)
 soup = BeautifulSoup(req.text, "html.parser")
 # Finding the main title tag.
-title = soup.find("table", class_="table-01 table-length-1")
+# title = soup.find("table", class_="table-01 table-length-1")
+title = soup.find("div",class_="table-container")
+if title  is None:
+        exit(0)
+liste = title.text
+#        print(len(line))
+suchstring = "DE-LU"
+if suchstring in liste:
+        print("liste enthaelt", suchstring)
+else:
+    print("leider nichts gefunden")
+    exit(0)
+suchstring = "15min"
+if suchstring in liste:
+        print("liste enthaelt", suchstring)
+else:
+    print("leider nichts gefunden")
+    exit(0)
+suchstring = mydate.strftime("%d %B %Y")
+if suchstring in liste:
+    print("liste enthaelt", suchstring)
+else:
+    print("leider nichts gefunden")
+    exit(0)
+title = soup.find("table")
 if title  is None:
         print ('keine Werte gefunden')
 else:
         line = title.contents
 #        print(len(line))
-
+        attrs = title.attrs
+        epexdate:str = attrs.get("data-head")
+        if epexdate != mydate.strftime("%d.%m.%y"):
+                exit(0)
         td = title.find_all("tr", class_="child")
 # für jedes Element
         y = 0;
@@ -47,10 +75,3 @@ else:
                 fp.write(str(y)+' '+str(x.contents[7].string)+'\n')
                 y=y+1
         fp.close()
-#main_url = "https://www.eex.com/de/marktdaten/market-data-hub/strom/futures#%7B%22snippetpicker%22%3A%2228%22%7D:~:text=Wochenende-,Tag,-Base"
-#req = requests.get(main_url,headers=headers)
-#print (req.text)
-#soup = BeautifulSoup(req.text, "html.parser")
-#td = soup.find_all("tr", class_="child")
-
-#print (td)
